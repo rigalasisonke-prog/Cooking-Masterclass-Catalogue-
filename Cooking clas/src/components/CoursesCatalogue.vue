9101a92f128bfdfda1fb7d554b01475bdc4294cc
rigalsisonke-prog <rigalasisonke@gmail.com>
2025-12-02 15:14:33 +0200

    i just did a toggle function for my save course  button

--- Cooking clas/src/components/CoursesCatalogue.vue
+++ Cooking clas/src/components/CoursesCatalogue.vue
@@ -8,7 +8,7 @@ export default{
                             title: "Mastering Italian Pasta",
                             price: 299,
                             skill_level: "Beginner",
-                            available: false,
+                            isSaved: false,
                             img: "https://online.tut.ac.za/hubfs/TUT_Exec%20chef%20blog.jpg"
                         },
                         {
@@ -16,7 +16,7 @@ export default{
                             title: "Sushi Rolling Essentials",
                             price: 350,
                             skill_level: "Intermediate",
-                            available: false,
+                            isSaved: true,
                             img: "https://www.silwood.co.za/wp-content/uploads/2024/03/silwood-school-of-cookery-chefs-in-training.jpg"
                         },
                         {
@@ -24,7 +24,7 @@ export default{
                             title: "French Pastry Techniques",
                             price: 420,
                             skill_level: "Advanced",
-                            available: false,
+                            isSaved: true,
                             img: "https://www.expatcapetown.com/images/cookingclasses-2.jpg"
                         },
                         {
@@ -32,11 +32,19 @@ export default{
                             title: "Vegan Cooking Fundamentals",
                             price: 250,
                             skill_level: "Beginner",
-                            available: false,
+                            isSaved: false,
                             img: "https://olivebranchcooking.co.za/wp-content/uploads/2024/02/Chef-Margy-1229x1536.jpg"
                         }
                     ]
                 }
+            },
+            methods:{
+                    toggleSave(courseID){
+                        const courseToSave = this.courses.find(course => course.id === courseID)
+                        if (courseToSave) {
+                            courseToSave.isSaved = !courseToSave.isSaved
+                        }
+                    }
             }
         }
 </script>
@@ -48,7 +56,7 @@ export default{
                 <h5 class="card-title">{{course.title}}</h5>
                 <p class="card-text">{{course.price}}</p>
                 <p class="card-text">{{course.skill_level}}</p>
-                <a href="#" class="btn btn-primary">{{course.available}}</a>
+                <a href="#" :class="['btn', course.isSaved?'btn-success' : 'btn-danger']" @click="toggleSave(course.id)">{{ course.isSaved ? 'course saved' : 'save course' }}</a>
             </div>
         </div>
     </div>
